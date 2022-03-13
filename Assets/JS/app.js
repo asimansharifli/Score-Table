c28502ae8cb8c0f9cd5fc61b972624d44fd1051a
asimansharifli <asiman.sharifli@gmail.com>
2022-03-14 00:05:26 +0400

    Score Table

---
+++ Assets/JS/app.js
@@ -0,0 +1,36 @@
+document.getElementById('number').onclick = function changeContent() {
+    let value = parseInt(document.getElementById('number').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('number').value = value;
+}
+document.getElementById('numbers').onclick = function changeContents() {
+    let value = parseInt(document.getElementById('numbers').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numbers').value = value;
+}
+document.getElementById('numbert').onclick = function changeContentt() {
+    let value = parseInt(document.getElementById('numbert').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numbert').value = value;
+}
+document.getElementById('numberfr').onclick = function changeContentfr() {
+    let value = parseInt(document.getElementById('numberfr').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numberfr').value = value;
+}
+document.getElementById('numberfs').onclick = function changeContentfs() {
+    let value = parseInt(document.getElementById('numberfs').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numberfs').value = value;
+}
+document.getElementById('numberss').onclick = function changeContentss() {
+    let value = parseInt(document.getElementById('numberss').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('numberss').value = value;
+}
